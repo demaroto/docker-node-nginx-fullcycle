23475d89bbe9194734556e1cf6459f63d0019c41
Wildemar Barbosa <demaroto69@gmail.com>
2024-06-01 22:14:52 -0300

    feat: add healthcheck and dockerize

---
+++ app/config/database.js
@@ -0,0 +1,10 @@
+const config = {
+    mysql : {
+        host: "db",
+        database: "appdb",
+        user: "root",
+        password: "root123"
+    }
+}
+
+module.exports = { config } 
--- app/server.js
+++ app/server.js
@@ -1,11 +1,27 @@
 const express = require('express')
 const app = express()
 const PORT = 3000
+const { connection } = require('./db/mysql')
+const PeopleRepository = require('./repositories/people');
+app.get('/', async (req, res) => { 
+    const name = "Pessoa " + Math.floor(Math.random() * (10000 - 1 + 1) + 5);
+    const peopleRepository = new PeopleRepository(connection);
+    await peopleRepository.createPeople(name);
+    const people = await peopleRepository.getPeople();
+   
+    let table = `<table style="width: 100%"><thead><th>ID</th><th>Nome</th></thead><tbody>`;
 
-app.get('/', (req, res) => {
-    res.json({server: 'ok'})
-})
+    
+    people.map((people) => {
+        table += `<tr><td>${people.id}</td><td style="text-align: center">${people.name}</td></tr>`
+    })
+    
+    
 
-app.listen(PORT, () => {
-    console.log(`listening on port ${PORT}`)
-})
+    table += `</tbody></table>`
+
+    res.send("<h1>Full Cycle Rocks!</h1>" + table);
+
+});
+
+app.listen(PORT, () => { console.log(`listening on port ${PORT}`) })
